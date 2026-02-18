80b9ca7b36c4dc914e651d8741d8339884145cb7
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2026-02-18 13:17:25 -0400

    feat: adicionado rota para logout

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'pages::home');
 Route::livewire('/login', 'pages::autenticacao.login')->name('autenticacao.login');
+Route::get('/logout', function () {
+    Auth::logout();
+    return redirect('/');
+});
 Route::livewire('/cadastro', 'pages::autenticacao.cadastro')->name('autenticacao.cadastro');
 Route::livewire('/dashboard', 'pages::autenticado.dashboard')->name('autenticado.dashboard');
 Route::prefix('contas-bancarias')->group(function () {
